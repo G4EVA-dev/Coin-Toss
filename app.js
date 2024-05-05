1117b6528cca2b1aa5d082b0b8b8d1035186c4ea
G4EVA-dev <tanzeglenn@gmail.com>
2024-05-05 01:28:57 +0100

    add html and css

---
+++ app.js
@@ -0,0 +1,10 @@
+// *****!Variables*****
+let coin = document.querySelector(".coin");
+let flipBtn = document.querySelector("#flipCoin");
+let resetBtn = document.querySelector("#resetGame");
+
+let heads = 0;
+let tails = 0;
+
+// !Game Logic
+
